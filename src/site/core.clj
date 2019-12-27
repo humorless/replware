02d8bc92cfb1729b0b7a64a970d4eddc1d5229f5
Laurence Chen <humorless@gmail.com>
2019-12-27 18:01:22 +0800

    add (create-header ) && use node-cljfmt to format

--- src/site/core.clj
+++ src/site/core.clj
@@ -1,53 +1,56 @@
 (ns site.core
   (:require [hiccup.page :as hp]))
 
-
 (defn- create-head
- "This will render html <head>"
- [title]
- [:head
+  "This will render html <head>"
+  [title]
+  [:head
    [:title title]
    [:meta {:charset "utf-8"}]
    [:meta
-    {:content "width=device-width, initial-scale=1, shrink-to-fit=no",
+    {:content "width=device-width, initial-scale=1, shrink-to-fit=no"
      :name "viewport"}]
    [:link
-    {:rel "shortcut icon",
-     :href "/assets/images/favicon.ico",
+    {:rel "shortcut icon"
+     :href "/assets/images/favicon.ico"
      :type "image/x-icon"}]
    [:link
-    {:rel "stylesheet",
+    {:rel "stylesheet"
      :href "https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css"}]])
 
 (defn- create-nav
   "render the <nav>"
   []
   [:nav.bg-white.relative.flex.justify-between.items-center
-    {:role "navigation"}
-    [:a.db.db-ns.mw4.link
-      {:href "/" :title "home"}
-      [:img.mh2.mv2 {:src "/assets/images/logo.png"}]]
-    [:ul.list.mv0.pr2.tr.flex-auto
-      [:li.di.mr3
-        [:a.link.dark-gray.hover-orange
-          {:href "/"} "Home"]]
-      [:li.di.mr3
-        [:a.link.dark-gray.hover-orange
-          {:href "/blog"} "Blog"]]
-      [:li.di.mr3
-        [:a.link.dark-gray.hover-orange
-          {:href "/about"} "About"]]]])
+   {:role "navigation"}
+   [:div.db.db-ns.h3]
+   [:ul.list.mv0.pr2.tr.flex-auto
+    [:li.di.mr3
+     [:a.link.dark-gray.hover-red
+      {:href "/"} "Home"]]
+    [:li.di.mr3
+     [:a.link.dark-gray.hover-red
+      {:href "/blog"} "Blog"]]
+    [:li.di.mr3
+     [:a.link.dark-gray.hover-red
+      {:href "/about"} "About"]]]])
 
 (defn- create-header
   "render the <header>"
   []
-  [:header])
+  [:header.bg-dark-gray.pv1.pv2-ns.lh-copy.flex.justify-between.items-center
+   [:a.ml4.db.db-ns.mw5.link
+    {:href "/" :title "home"}
+    [:img.mh2.mv2 {:src "/assets/images/logo.png"}]]
+   [:h1.pr5.pv4.white
+    [:div "bring " [:span.yellow "your ideas"]]
+    [:div "into " [:span.light-blue.center "software"]]]])
 
 (defn- create-main
   "render the <main>"
   [content]
   [:main
-    content])
+   content])
 
 (defn- create-footer
   "render the <footer>"
@@ -58,15 +61,16 @@
   "This will render html <body>"
   [data]
   [:body.helvetica.dark-gray
-    (create-nav)
-    (create-header)
-    (create-main
-      (-> data :entry :content) )
-    (create-footer)])
+   (create-nav)
+   (create-header)
+   (create-main
+    (-> data :entry :content))
+   (create-footer)])
 
 (defn page
   "the page renderer"
   [data]
-  (hp/html5 {:lang "en"}
-    (create-head "replware")
-    (create-body data)))
+  (hp/html5
+   {:lang "en"}
+   (create-head "replware")
+   (create-body data)))
