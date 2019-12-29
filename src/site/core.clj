1b1d113f81c137ce6306f9656d24a6f44270f80c
Laurence Chen <humorless@gmail.com>
2019-12-29 19:48:34 +0800

    improve responsive design / add footer

--- src/site/core.clj
+++ src/site/core.clj
@@ -24,7 +24,7 @@
   [:nav.bg-white.flex.justify-between.items-center
    {:role "navigation"}
    [:div.db.db-ns.h3]
-   [:ul.list.mv0.pr5.tr.flex-auto
+   [:ul.list.mv0.pr4.pr5-ns.tr.flex-auto
     [:li.di.mr3
      [:a.link.dark-gray.hover-red
       {:href "/"} "Home"]]
@@ -35,11 +35,11 @@
 (defn- create-header
   "render the <header>"
   []
-  [:header.bg-dark-gray.pv1.pv2-ns.lh-copy.flex.justify-between.items-center
-   [:a.ml4.db.db-ns.mw5.link
+  [:header.bg-dark-gray.pv1.pv2-ns.lh-copy-ns.flex.justify-between.items-center
+   [:a.ml4-ns.db.db-ns.mw4.mw5-ns.link
     {:href "/" :title "home"}
     [:img.mh2.mv2 {:src "/assets/logo/logo_transparent_background.png"}]]
-   [:h1.pr5.pv4.white
+   [:h1.pr4.pr5-ns.pv4-ns.white
     [:div "turn " [:span.yellow "your ideas"]]
     [:div "into " [:span.light-blue.center "software"]]]])
 
@@ -61,7 +61,7 @@
     [:main.w-100.mw8.center.ph3.pv4
      [:section
       [:h2 "who"]
-      [:img.mw5.relative.fr {:src "/assets/images/photo.jpg"}]
+      [:img.mw4.mw5-ns.relative.fr {:src "/assets/images/photo.jpg"}]
       who]
      [:section
       [:h2 "services"]
@@ -79,7 +79,19 @@
 (defn- create-footer
   "render the <footer>"
   []
-  [:footer])
+  [:footer.db-ns
+   [:div.ph3.pv4.flex.justify-between
+    [:div.db.db-ns]
+    [:ul.mv0.pr4.pr5-ns.list
+     [:li.f5.di.mr3
+      [:a.link.dark-gray.hover-red
+       {:href "https://twitter.com/humorless"} "Twitter"]]
+     [:li.f5.di.mr3
+      [:a.link.dark-gray.hover-red
+       {:href "https://linkedin.com/in/humorless"} "Linkedin"]]
+     [:li.f5.di.mr3
+      [:a.link.dark-gray.hover-red
+       {:href "https://github.com/humorless"} "Github"]]]]])
 
 (defn- create-body
   "This will render html <body>"
